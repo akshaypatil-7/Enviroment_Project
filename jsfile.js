4df78dfbdc38fc5dbeb1063905566a699d52fbd8
akshaypatil-7 <78476997+akshaypatil-7@users.noreply.github.com>
2021-04-22 17:44:15 +0530

    Add files via upload

---
+++ jsfile.js
@@ -0,0 +1,19 @@
+ function video(){
+     open("https://youtu.be/ydbT5XH24cc");
+  }
+  
+  function info(){
+     open("https://en.m.wikipedia.org/wiki/Nature");
+  }
+  
+  function home(){
+     alert("Well~Come This Is Home Page :)");
+  }
+  
+   function contact(){
+     alert("Well~Come This Is Contacts Page :)");
+  }
+  
+   function service(){
+     alert("Well~Come This Is Service Page :)");
+  }
